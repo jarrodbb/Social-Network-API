c1ff68c58ab29c5bb4137c82de692a5ff20685c4
Jarrod Blanning <jarrod.blanning@gmail.com>
2023-09-21 13:35:55 +1000

    User API Routes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+
+const {
+  getAllUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addAFriend,
+  deleteAFriend,
+} = require("../../controllers/userController");
+
+router.route("/").get(getAllUsers).post(createUser);
+
+router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
+
+router
+  .route("/:userId/friends/:friendId")
+  .post(addAFriend)
+  .delete(deleteAFriend);
+
+module.exports = router;
