0caa60eeb0a77ba26c9544bbb5b931ac02ced647
Jarrod Blanning <jarrod.blanning@gmail.com>
2023-09-21 13:52:25 +1000

    Thought API Routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,26 @@
+const router = require("express").Router();
+
+const {
+  getThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  addAReaction,
+  deleteAReaction,
+} = require("../../controllers/thoughtControllers");
+
+router.route("/").get(getThoughts).post(createThought);
+
+router
+  .route("/:thoughtId")
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+router
+  .route("/:thoughtId/reactions")
+  .post(addAReaction)
+  .delete(deleteAReaction);
+
+module.exports = router;
